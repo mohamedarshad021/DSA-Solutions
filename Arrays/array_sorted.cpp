bdf796018ea8aa755aaa45896de2690d0743abb2
mohamedarshad021 <mohamed2007arshad@gmail.com>
2025-12-30 00:21:05 +0530

    Create array_sorted.cpp

---
+++ Arrays/array_sorted.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    bool sorted = true;
+    for(int i=0;i<n-1;i++) {
+        if(a[i] > a[i+1]) {
+            sorted = false;
+            break;
+        }
+    }
+    if(sorted) cout<<"Sorted";
+    else cout<<"Not Sorted";
+    return 0;
+}
