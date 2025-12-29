554912f3b7f8525e3afe6f45ddf16b10c4ef0954
mohamedarshad021 <mohamed2007arshad@gmail.com>
2025-12-30 00:21:30 +0530

    Add function to convert string to uppercase

---
+++ Strings/to_uppercase.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin,s);
+    for(char &c : s)
+        if(c>='a' && c<='z')
+            c = c - 32;
+    cout<<s;
+    return 0;
+}
