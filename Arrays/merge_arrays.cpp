ebcea4416f64a44d636749b30bc74fd3906b3a98
mohamedarshad021 <mohamed2007arshad@gmail.com>
2025-12-30 00:20:49 +0530

    Add merge_arrays.cpp to merge two arrays
    This program merges two arrays by taking input for their sizes and elements, then outputs the elements of both arrays.

---
+++ Arrays/merge_arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1,n2;
+    cin>>n1>>n2;
+    int a[n1], b[n2];
+
+    for(int i=0;i<n1;i++) cin>>a[i];
+    for(int i=0;i<n2;i++) cin>>b[i];
+
+    for(int i=0;i<n1;i++) cout<<a[i]<<" ";
+    for(int i=0;i<n2;i++) cout<<b[i]<<" ";
+    return 0;
+}
